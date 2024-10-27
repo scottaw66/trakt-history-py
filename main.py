53bb28f33488e8a3f394da3ac4da3391adc7e3e8
Scott Willsey <sunny.flame0794@fastmail.com>
2024-10-27 09:19:35 -0700

    Starts using trakt API through my forked pytrakt to get OAuth token and start playing with queries

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #!/Users/scott/Scripts/python/venv/bin/python
 import os
+import argparse
 from dotenv import load_dotenv
+from trakt import init
+import trakt.core
+from trakt.movies import get_recommended_movies
 
 def main():
     load_dotenv()
@@ -10,8 +14,25 @@ def main():
     APPLICATION_ID = os.getenv("APPLICATION_ID")
     USERNAME_ID = os.getenv("USERNAME_ID")
     
-    print(f'{CLIENT_ID}\n{CLIENT_SECRET}\n{APPLICATION_ID}\n{USERNAME_ID}')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--auth', action='store_true', help='Perform OAuth authorization')
+    args = parser.parse_args()
     
+    trakt.core.AUTH_METHOD = trakt.core.OAUTH_AUTH  # Set the auth method to OAuth
+    trakt.APPLICATION_ID = APPLICATION_ID
+    
+    if args.auth:
+        init(USERNAME_ID, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, store=True)
+        exit()
+        
+    from trakt.users import User
+    me = User(USERNAME_ID)
+    print(f"Me: {me}\n\n")
+    print(f"Watched shows: {me.watched_shows}\n\n")
+    print(f"Show watchlist: {me.watchlist_shows}\n\n")
+    print(f"Movie watchlist: {me.watchlist_movies}\n\n")
+    print(f"Stats: {me.get_stats}\n\n")
+
 
 if __name__ == "__main__":
     main()
